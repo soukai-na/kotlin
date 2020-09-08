66f3bf3d8570d870dea1cac737f2f05bc99c7242
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-09-08 11:55:14 +0300

    ++ FIR analysis test data fixed

--- compiler/testData/diagnostics/tests/generics/Projections.fir.kt
+++ compiler/testData/diagnostics/tests/generics/Projections.fir.kt
@@ -37,16 +37,16 @@ fun testInOut() {
 
     Inv<Int>().f(1)
     (null as Inv<in Int>).f(1)
-    (null as Inv<out Int>).f(1) // !!
+    (null as Inv<out Int>).<!INAPPLICABLE_CANDIDATE!>f<!>(1) // !!
     (null as Inv<*>).<!INAPPLICABLE_CANDIDATE!>f<!>(1) // !!
 
     Inv<Int>().inf(1)
     (null as Inv<in Int>).inf(1)
-    (null as Inv<out Int>).inf(1) // !!
+    (null as Inv<out Int>).<!INAPPLICABLE_CANDIDATE!>inf<!>(1) // !!
     (null as Inv<*>).<!INAPPLICABLE_CANDIDATE!>inf<!>(1) // !!
 
     Inv<Int>().outf()
-    checkSubtype<Int>((null as Inv<in Int>).outf()) // Type mismatch
+    <!INAPPLICABLE_CANDIDATE!>checkSubtype<!><Int>((null as Inv<in Int>).outf()) // Type mismatch
     (null as Inv<out Int>).outf()
     (null as Inv<*>).outf()
 

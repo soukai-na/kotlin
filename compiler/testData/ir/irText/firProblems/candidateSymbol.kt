831daccd77cc99775d874a1d61b7cdbc094f7e74
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-09-09 11:53:01 +0300

    ++ [FIR2IR] Add problematic test with incorrect type argument

--- compiler/testData/ir/irText/firProblems/candidateSymbol.kt
+++ compiler/testData/ir/irText/firProblems/candidateSymbol.kt
@@ -11,6 +11,11 @@ interface FirSymbolOwner<E> where E : FirSymbolOwner<E>, E : FirDeclaration {
     val symbol: AbstractFirBasedSymbol<E>
 }
 
+interface FirCallableMemberDeclaration<F : FirCallableMemberDeclaration<F>> : FirSymbolOwner<F>, FirDeclaration {
+    override val symbol: AbstractFirBasedSymbol<F>
+}
+
 fun foo(candidate: Candidate) {
     val me = candidate.symbol.fir
+    if (me is FirCallableMemberDeclaration<*> && me.symbol != null) {}
 }
